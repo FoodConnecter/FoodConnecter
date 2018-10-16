91d05e393e92cdd33656f48f1ce140d6e3b8499b
Yamagn <syanekenpon@gmail.com>
2018-10-17 00:58:51 +0900

    修正

---
+++ Food_Connecter/Food_Connecter/Views/eventPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+namespace Food_Connecter
+{
+    public partial class eventPage : ContentPage
+    {
+        public eventPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (App.Authenticator.user == null)
+            {
+                listView.IsPullToRefreshEnabled = false;
+                await DisplayAlert("ログインしてください", "", "閉じる");
+                return;
+            }
+            listView.IsRefreshing = true;
+            var res = await App.client.GetAsync("https://samplefood2.azurewebsites.net/api/eventview?pref=" + kanriPage.userInfo.Pref);
+            var json = await res.Content.ReadAsStringAsync();
+            Console.WriteLine(json);
+            var eventList = new List<eventModel>();
+            try
+            {
+                eventList = JsonConvert.DeserializeObject<List<eventModel>>(json);
+            }
+            catch
+            {
+                return;
+            }
+            
+            listView.ItemsSource = eventList;
+            listView.IsRefreshing = false;
+        }
+
+        async void refreshList(object sender, EventArgs e)
+        {
+            var res = await App.client.GetAsync("https://samplefood2.azurewebsites.net/api/eventview?pref=" + kanriPage.userInfo.Pref);
+            var json = await res.Content.ReadAsStringAsync();
+            var eventList = new List<eventModel>();
+            try
+            {
+                eventList = JsonConvert.DeserializeObject<List<eventModel>>(json);
+            }
+            catch
+            {
+                return;
+            }
+            listView.ItemsSource = eventList;
+
+            listView.EndRefresh();
+        }
+
+        async void manageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddEventPage());
+        }
+
+        async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem == null)
+            {
+                return;
+            }
+
+            var info = (eventModel)e.SelectedItem;
+            if(info.Userid == App.Authenticator.user.UserId)
+            {
+                await Navigation.PushAsync(new eventManagePage
+                {
+                    BindingContext = info
+                });
+            }
+            else
+            {
+                await Navigation.PushAsync(new eventDetailpage
+                {
+                    BindingContext = info
+                });
+            }
+        }
+
+        async void viewEvent_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new viewEventPage());
+        }
+    }
+}
---
+++ Food_Connecter/Food_Connecter/Views/uketoriListPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+using Xamarin.Forms;
+
+namespace Food_Connecter
+{
+    public partial class uketoriListPage : ContentPage
+    {
+        public class users
+        {
+            [JsonProperty("username")]
+            public string userName { get; set; }
+        }
+        public uketoriListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (App.Authenticator.user == null)
+            {
+                listView.IsPullToRefreshEnabled = false;
+                await DisplayAlert("ログインしてください", "", "閉じる");
+                return;
+            }
+            Stack.IsVisible = true;
+
+            try
+            {
+                var res = await App.client.GetAsync(Constants.ApplicationURL + "/api/getchatlist?username=" + kanriPage.userInfo.UserName);
+                var json = JsonConvert.DeserializeObject<List<users>>(await res.Content.ReadAsStringAsync());
+                Console.WriteLine(await res.Content.ReadAsStringAsync());
+                listView.ItemsSource = json;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                await DisplayAlert("通信に失敗しました", "", "閉じる");
+            }
+
+            Stack.IsVisible = false;
+        }
+
+        async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            await Navigation.PushAsync(new ChatPage
+            {
+                BindingContext = ((users)e.SelectedItem).userName
+            });
+        }
+    }
+}
