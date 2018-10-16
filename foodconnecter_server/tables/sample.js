f2c004f2ac4b0faf91e803fdc5fa12e2c00311bb
sotasunohara <sotasuno0175@gmail.com>
2018-10-17 00:58:32 +0900

    サーバ追加

---
+++ foodconnecter_server/tables/sample.js
@@ -0,0 +1,42 @@
+
+var table = module.exports = require('azure-mobile-apps').table();
+
+function queryContextForEmail(context) {
+    return context.user.getIdentity().then((data) => {
+        context.query.where({ emailAddress: data.microsoftaccount.claims.emailaddress });
+        return context.execute();
+    });
+}
+
+/**
+* Adds the email address from the claims to the context item - used for
+* insert operations
+* @param {Context} context the operation context
+* @returns {Promise} context execution Promise
+*/
+function addEmailToContext(context) {
+    return context.user.getIdentity().then((data) => {
+        context.item.emailAddress = data.microsoftaccount.claims.emailaddress;
+        return context.execute();
+    });
+}
+
+// Configure specific code when the client does a request.
+// READ: only return records that belong to the authenticated user.
+table.read(queryContextForEmail);
+
+// CREATE: add or overwrite the userId based on the authenticated user.
+table.insert(addEmailToContext);
+
+// UPDATE: only allow updating of records that belong to the authenticated user.
+table.update(queryContextForEmail);
+
+// DELETE: only allow deletion of records that belong to the authenticated user.
+table.delete(queryContextForEmail);
+
+module.exports = table;
+// table.read(function (context) {
+//     return context.execute();
+// });
+
+// table.read.use(customMiddleware, table.operation);
